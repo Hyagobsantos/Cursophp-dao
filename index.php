14044269a25d4c86625296a3c681620d28005c79
Hyago b <hyagobezerra64@gmail.com>
2020-09-11 16:35:48 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -10,12 +10,27 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(7);
+//echo $root;
 
-$root = new Usuario();
 
-$root->loadById(7);
 
-echo $root;
+//carrega varios usuarios lista
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de usuarios buscando pelo login, definindo pelo usuarios buscando 
+//$search = Usuario::search("hy");
+//echo json_encode($search);
+
+//carraga um usuario usando o login e a senha 
+$usuario = new Usuario();
+$usuario->login("caio" ,"bicha");
+
+echo $usuario;
+
 
 
 
